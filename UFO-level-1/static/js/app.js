33b208020b80e1b365039163630200f561c96659
Rob G <popestarkiller@icloud.com>
2021-06-22 09:55:23 -0700

    added the table and started the button coding

--- UFO-level-1/static/js/app.js
+++ UFO-level-1/static/js/app.js
@@ -2,3 +2,27 @@
 var tableData = data;
 
 // YOUR CODE HERE!
+var tbody = d3.select("tbody");
+
+tableData.forEach(function(ufoReport)   {
+    
+    var row = tbody.append("tr");
+    Object.entries(ufoReport).forEach(function([key, value]) {
+        var cell = row.append("td");
+        cell.text(value);
+    });
+});
+
+var form = d3.select("#form-group");
+var button = d3.select("#filter-btn");
+
+button.on("click", runEnter);
+form.on("submit",runEnter);
+
+function runEnter() {
+    d3.event.preventDefault();
+    var inputElement = d3.select("#date-time");
+    var inputValue = inputElement.property("value");
+    console.log(inputValue);
+    var filteredData = tableData.filter(date => parseDate(date.datetime) === parseDate(inputValue));
+    console.log(filteredData)}
