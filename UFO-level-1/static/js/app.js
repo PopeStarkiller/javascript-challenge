8299f36352c12de8a052409d440e69e5d0596818
Rob G <popestarkiller@icloud.com>
2021-06-22 15:16:32 -0700

    finished most of level one with a few minor bugs to tweak and code cleaning to do.

--- UFO-level-1/static/js/app.js
+++ UFO-level-1/static/js/app.js
@@ -13,16 +13,35 @@ tableData.forEach(function(ufoReport)   {
     });
 });
 
-var form = d3.select("#form-group");
+// var form = d3.select("#form-group");
 var button = d3.select("#filter-btn");
 
 button.on("click", runEnter);
-form.on("submit",runEnter);
+// form.on("submit",runEnter);
 
 function runEnter() {
     d3.event.preventDefault();
-    var inputElement = d3.select("#date-time");
+    tbody.selectAll('tr').remove();
+    var inputElement = d3.select("#datetime");
     var inputValue = inputElement.property("value");
-    console.log(inputValue);
-    var filteredData = tableData.filter(date => parseDate(date.datetime) === parseDate(inputValue));
-    console.log(filteredData)}
+    // console.log(inputValue);
+    if (inputValue !== "") {
+        var filteredData = tableData.filter(input => input.datetime === inputValue);
+    }
+    else 
+        {var filteredData = tableData;
+    }
+    // tbody.append(filteredData)
+    // var filteredData = tableData.filter(date => parseDate(date.datetime) === parseDate(inputValue));
+    filteredData.forEach(function(ufoReport)   {
+    
+        var row = tbody.append("tr");
+        Object.entries(ufoReport).forEach(function([key, value]) {
+            var cell = row.append("td");
+            cell.text(value);
+        });
+    });
+    
+    
+    
+    console.log(filteredData)};
